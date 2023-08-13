27bab8c91ef675831d0a98304e6bd37424667998
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-08-13 19:34:35 +0530

    Update DataStreamOperations.java

--- src/com/personal/flink/DataStreamOperations.java
+++ src/com/personal/flink/DataStreamOperations.java
@@ -1,1 +1,47 @@
+package ee.ut.cs.dsg;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkStreamSQLExamples {
+
+    public static void main(String[] args) throws Exception
+    {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> file = env.readTextFile("C:\\razarapersonal\\flink-programming\\src\\main\\resources\\emp.csv");
+
+        //----------------Filter function--------------------------
+        SingleOutputStreamOperator<String> empAfterFilter = file.filter(new FilterFunction<String>() {
+            @Override
+            public boolean filter(String s) throws Exception {
+                return !s.contains("php");
+            }
+        });
+
+        //--------------keyby------------------
+        KeyedStream<String, String> keyedStream = empAfterFilter.keyBy(value -> value.split(",")[2]);
+
+        keyedStream.writeAsText("C:\\razarapersonal\\flink-programming\\src\\main\\resources\\grp-by-sink.csv", FileSystem.WriteMode.OVERWRITE);
+
+        //-----------Map function---------------
+
+        /*SingleOutputStreamOperator<Integer> ids = file.map(new MapFunction<String, Integer>() {
+            @Override
+            public Integer map(String s) throws Exception {
+                return Integer.parseInt(s.split(",")[0]) + 100;
+            }
+        });
+
+        ids.writeAsText("C:\\razarapersonal\\flink-programming\\src\\main\\resources\\ids-sink.csv", FileSystem.WriteMode.OVERWRITE);
+*/
+        env.execute("Example Flink DataStream Programming");
+    }
+}
 
